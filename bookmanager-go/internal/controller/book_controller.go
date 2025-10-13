8747be43cc9a31c293d310891984c8c9ce59fa54
DevSchmied <DevSchmied@yahoo.com>
2025-10-13 16:19:20 +0200

    feat(controller/book): register initial /books route for GetAllBooks handler

--- bookmanager-go/internal/controller/book_controller.go
+++ bookmanager-go/internal/controller/book_controller.go
@@ -13,6 +13,14 @@ type BookController struct {
 	DB *gorm.DB
 }
 
+// RegisterRoutes sets up all book-related routes.
+func (bc *BookController) RegisterRoutes(r *gin.Engine) {
+	books := r.Group("/books")
+	{
+		books.GET("/", bc.GetAllBooks) // Read all
+	}
+}
+
 // GetAllBooks returns all books as JSON.
 func (bc *BookController) GetAllBooks(c *gin.Context) {
 	var books []model.Book
