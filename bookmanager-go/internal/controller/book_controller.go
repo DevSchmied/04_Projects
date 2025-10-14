89a577d03af4485d4e9cc58d48ff82f29e307b1d
DevSchmied <DevSchmied@yahoo.com>
2025-10-14 16:07:14 +0200

    feat(04_PROJECTS/bookmanager-go): extend BookController with CRUD route definitions and placeholder methods

--- bookmanager-go/internal/controller/book_controller.go
+++ bookmanager-go/internal/controller/book_controller.go
@@ -17,7 +17,11 @@ type BookController struct {
 func (bc *BookController) RegisterRoutes(r *gin.Engine) {
 	books := r.Group("/books")
 	{
-		books.GET("/", bc.GetAllBooks) // Read all
+		books.GET("/", bc.GetAllBooks)             // Read all
+		books.GET("/:id", bc.GetBookByID)          // Read one
+		books.POST("/add", bc.AddBook)             // Create
+		books.PUT("/:id/edit", bc.UpdateBook)      // Update
+		books.DELETE("/delete/:id", bc.DeleteBook) // Delete
 	}
 }
 
@@ -30,3 +34,8 @@ func (bc *BookController) GetAllBooks(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, books)
 }
+
+func (bc *BookController) GetBookByID(c *gin.Context) {}
+func (bc *BookController) AddBook(c *gin.Context)     {}
+func (bc *BookController) UpdateBook(c *gin.Context)  {}
+func (bc *BookController) DeleteBook(c *gin.Context)  {}
