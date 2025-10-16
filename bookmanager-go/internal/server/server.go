4edb6042cf75969ebc42fd6ffd34c68ba051b6c8
DevSchmied <DevSchmied@yahoo.com>
2025-10-16 12:02:11 +0200

    fix(04_Projects/bookmanager-go):saved unsaved changes after previous commit

--- bookmanager-go/internal/server/server.go
+++ bookmanager-go/internal/server/server.go
@@ -12,7 +12,8 @@ func StartWebServer(db *gorm.DB) {
 	router := gin.Default()
 
 	bookController := controller.BookController{DB: db}
-	bookController.RegisterRoutes(router)
+	bookController.RegisterRoutes(router) // register all /books routes
 
+	// start the HTTP server on localhost:8080
 	router.Run("localhost:8080")
 }
