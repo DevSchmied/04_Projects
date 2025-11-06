9fbf4664bd7949f6b1b87f0aacee0dde64beaaa3
DevSchmied <DevSchmied@yahoo.com>
2025-11-06 12:15:42 +0100

    feat(04_Projects/bookmanager-go/controller): implement IDSearch strategy for finding books by ID

--- bookmanager-go/internal/controller/search_strategy.go
+++ bookmanager-go/internal/controller/search_strategy.go
@@ -2,10 +2,23 @@ package controller
 
 import (
 	"bookmanager-go/internal/model"
+	"strconv"
 
 	"gorm.io/gorm"
 )
 
 type SearchStrategy interface {
 	Search(db *gorm.DB, value string) (*model.Book, error)
+}
+
+type IDSearch struct{}
+
+func (s IDSearch) Search(db *gorm.DB, value string) (*model.Book, error) {
+	var book model.Book
+	id, err := strconv.Atoi(value)
+	if err != nil {
+		return nil, err
+	}
+	err = db.First(&book, id).Error
+	return &book, err
 }
