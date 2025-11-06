735775a853d9c4448d38e020c86d77332690f23e
DevSchmied <DevSchmied@yahoo.com>
2025-11-06 12:09:39 +0100

    feat(04_Projects/bookmanager-go/controller): add SearchStrategy interface definition

--- bookmanager-go/internal/controller/search_strategy.go
+++ bookmanager-go/internal/controller/search_strategy.go
@@ -1,1 +1,11 @@
 package controller
+
+import (
+	"bookmanager-go/internal/model"
+
+	"gorm.io/gorm"
+)
+
+type SearchStrategy interface {
+	Search(db *gorm.DB, value string) (*model.Book, error)
+}
