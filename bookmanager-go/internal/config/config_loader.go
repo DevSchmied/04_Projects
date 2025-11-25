fbe78b8edc6a42a18eb88e09818ea8246a17d2f0
DevSchmied <DevSchmied@yahoo.com>
2025-11-25 13:44:59 +0100

    fix(04_Projects/bookmanager-go/config): add LoadConfig method with retry mechanism using recursive approach for practice

--- bookmanager-go/internal/config/config_loader.go
+++ bookmanager-go/internal/config/config_loader.go
@@ -1,8 +1,12 @@
 package config
 
+import (
+	"fmt"
+)
+
 // ConfigLoader defines an interface for loading configuration.
 type ConfigLoader interface {
-	LoadConfig(attempts, maxAttempts int) error
+	LoadConfig(attempt, maxAttempts int) error
 }
 
 // EnvLoader loads environment variables using a custom load function.
@@ -17,4 +21,13 @@ func NewEnvLoader(path string, loadFunc func() error) *EnvLoader {
 		FilePath: path,
 		Load:     loadFunc,
 	}
+}
+
+// LoadConfig loads configuration recursively with a retry mechanism.
+func (e *EnvLoader) LoadConfig(attempt, maxAttempts int) error {
+	if attempt >= maxAttempts {
+		return fmt.Errorf("failed to load configuration after %d attempts", maxAttempts)
+	}
+
+	return e.LoadConfig(attempt, maxAttempts)
 }
