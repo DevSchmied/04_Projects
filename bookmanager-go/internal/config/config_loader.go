b5c7e5748ffd724be1766bf398ef1ab7b7d2db2d
DevSchmied <DevSchmied@yahoo.com>
2025-11-25 13:18:34 +0100

    feat(internal/config): add EnvLoader struct for environment variable loading

--- bookmanager-go/internal/config/config_loader.go
+++ bookmanager-go/internal/config/config_loader.go
@@ -4,3 +4,9 @@ package config
 type ConfigLoader interface {
 	LoadConfig(attempts, maxAttempts int) error
 }
+
+// EnvLoader loads environment variables using a custom load function.
+type EnvLoader struct {
+	FilePath string
+	Load     func() error
+}
