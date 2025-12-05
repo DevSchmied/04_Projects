9b3cee6b5ed31f6ac71efc457e5e793cc914c84a
DevSchmied <DevSchmied@yahoo.com>
2025-12-05 11:54:10 +0100

    feat(04_Projects/bookmanager-go/cache): introduce RedisBookCache with constructor and cache constants

--- bookmanager-go/internal/cache/book_cache.go
+++ bookmanager-go/internal/cache/book_cache.go
@@ -1,1 +1,17 @@
 package cache
+
+import "time"
+
+const (
+	bookListKey = "book:list"
+	bookListTTL = 60 * time.Second
+)
+
+// implement interface BookCache
+type RedisBookCache struct {
+	rdb *RedisClient
+}
+
+func NewRedisBookCache(rdb *RedisClient) *RedisBookCache {
+	return &RedisBookCache{rdb: rdb}
+}
